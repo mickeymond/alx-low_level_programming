9b91e49df8c8310c7681c5eaf47efeb4011e06c2
mickeymond <33749130+mickeymond@users.noreply.github.com>
2023-04-09 11:48:38 -0700

    Write a function that checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endianness = (char *)&num;
+
+	if (*endianness == 1)
+		return (1);
+
+	return (0);
+}
